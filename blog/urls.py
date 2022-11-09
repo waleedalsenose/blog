e76c54387334bebd847002860347b9953bb2846e
Waleed <willy-alsenose@hotmail.com>
2022-11-09 11:16:18 +0200

    adding CRUD functionality to our blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BlogListView,BlogDetailView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'), #new
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'), #new
+    path('post/new/', BlogCreateView.as_view(), name='post_new'), #new
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), #new
     path('', BlogListView.as_view(), name='home')
 ]
